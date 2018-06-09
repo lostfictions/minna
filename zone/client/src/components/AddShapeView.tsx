9abe7b4278abc2c8708cc43d410a2f28632d2308
s <lostfictions@users.noreply.github.com>
2018-06-09 17:32:32 -0400

    progress implementing manipulation with hammer + gestures

--- zone/client/src/components/AddShapeView.tsx
+++ zone/client/src/components/AddShapeView.tsx
@@ -4,6 +4,8 @@ import { computed, observable, action } from "mobx";
 
 import Draggable, { DraggableEventHandler } from "react-draggable";
 
+import Hammer from "hammerjs";
+
 import { Store } from "../Store";
 
 import { PolyType, Poly } from "../../../shared";
@@ -46,6 +48,37 @@ class Paper extends React.Component<{ model: PolyType }> {
     aspect: 1 / 1.4
   };
 
+  hammer: HammerManager | null = null;
+  ref = React.createRef<SVGGElement>();
+
+  componentDidMount() {
+    const h = new Hammer(this.ref.current!);
+    this.hammer = h;
+    h.add(new Hammer.Pan({ threshold: 0, pointers: 0 }));
+    h.add(new Hammer.Rotate({ threshold: 0 })).recognizeWith(h.get("pan"));
+    h.add(new Hammer.Pinch({ threshold: 0 })).recognizeWith(
+      [h.get("pan"), h.get("rotate")] as any /* bad typings */
+    );
+
+    h.add(new Hammer.Tap());
+
+    // h.on("panstart", this.onPanStart);
+    h.on("panmove", this.onPan);
+    h.on("panend", this.onPanEnd);
+    // h.on("rotatestart", this.onRotateStart);
+    // h.on("rotatemove", this.onRotate);
+    // h.on("pinchstart", this.onPinchStart);
+    // h.on("pinchmove", this.onPinch);
+    // h.on("tap", this.onTap);
+  }
+
+  componentWillUnmount() {
+    if (this.hammer) {
+      this.hammer.destroy();
+      this.hammer = null;
+    }
+  }
+
   @computed
   get svgPathString(): string {
     const ps = this.props.model.points;
@@ -54,6 +87,66 @@ class Paper extends React.Component<{ model: PolyType }> {
       .slice(1)
       .map(({ x, y }) => `L ${x} ${y}`)
       .join(" ")}`;
+  }
+
+  lastDeltaX = 0;
+  lastDeltaY = 0;
+  @action.bound
+  onPan(ev: HammerInput) {
+    this.transform.x += ev.deltaX - this.lastDeltaX;
+    this.transform.y += ev.deltaY - this.lastDeltaY;
+    this.lastDeltaX = ev.deltaX;
+    this.lastDeltaY = ev.deltaY;
+  }
+
+  onPanEnd = () => {
+    this.lastDeltaX = 0;
+    this.lastDeltaY = 0;
+  };
+
+  rotateInitialAngle!: number;
+  rotateInitialRotation!: number;
+  rotateInitialX!: number;
+  rotateInitialY!: number;
+
+  onRotateStart = (ev: HammerInput) => {
+    this.rotateInitialAngle = this.transform.angle;
+    // HACK: the event rotation seems to be an absolute value, not a relative
+    // one. even the example on the hammer frontpage is wrong?
+    this.rotateInitialRotation = ev.rotation;
+    this.rotateInitialX = this.transform.x;
+    this.rotateInitialY = this.transform.y;
+    // Alternately:
+    // this.rotateInitialAngle = this.transform.angle - ev.rotation;
+  };
+
+  @action.bound
+  onRotate(ev: HammerInput) {
+    const theta = (ev.rotation - this.rotateInitialRotation) * 0.0174533;
+    const cos = Math.cos(theta);
+    const sin = Math.sin(theta);
+    const deltaX = this.transform.x - ev.center.x;
+    const deltaY = this.transform.y - ev.center.y;
+
+    this.transform.x = cos * deltaX - sin * deltaY + ev.center.x;
+    this.transform.y = sin * deltaX + cos * deltaY + ev.center.y;
+    this.transform.angle = this.rotateInitialAngle + ev.rotation;
+  }
+
+  pinchInitialScale!: number;
+  onPinchStart = (ev: HammerInput) => {
+    this.pinchInitialScale = this.transform.scale;
+    this.onPinch(ev);
+  };
+
+  @action.bound
+  onPinch(ev: HammerInput) {
+    this.transform.scale = this.pinchInitialScale * ev.scale;
+  }
+
+  @action.bound
+  onTap(ev: HammerInput) {
+    //
   }
 
   onDragPoint: DraggableEventHandler = (_ev, data) => {
@@ -81,8 +174,11 @@ class Paper extends React.Component<{ model: PolyType }> {
 
     return (
       <g
+        ref={this.ref}
         style={{
-          transform: `translate3d(${x}px, ${y}px, 0) scale(${scale}, ${scale}) rotate(${angle}deg)`
+          transform: `translate(${x}px, ${y}px) scale(${scale}) rotate(${angle}deg)`
+          // transform: `translate(${x}px, ${y}px) scale(${scale}, ${scale}) rotate(${angle}deg)`
+          // transform: `translate3d(${x}px, ${y}px, 0) scale(${scale}, ${scale}) rotate(${angle}deg)`
         }}
         // // TODO: should be on child, here for simplicity for now
         // onTouchMoveCapture={ev => {
@@ -99,7 +195,7 @@ class Paper extends React.Component<{ model: PolyType }> {
           y={0}
           width={aspect}
           height={1}
-          onClick={this.onClickRect}
+          // onClick={this.onClickRect}
           fill={points.length === 0 ? color : "rgba(200, 200, 20, 0.3)"}
         />
         <path
@@ -109,8 +205,8 @@ class Paper extends React.Component<{ model: PolyType }> {
           strokeWidth={2}
         />
 
-        {points.map(({ x, y }, i) => (
-          <Point onDrag={this.onDragPoint} x={x} y={y} index={i} key={i} />
+        {points.map(({ x: pX, y: pY }, i) => (
+          <Point onDrag={this.onDragPoint} x={pX} y={pY} index={i} key={i} />
         ))}
       </g>
     );
